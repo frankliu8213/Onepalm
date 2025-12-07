23e04f117d62a3b5192384e512b8581b725b88da
fan liu <liufan66@126.com>
2025-12-07 15:52:19 +0800

    Initial commit: Dharma One Palm Sutra App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OnePalmApp from './OnePalmApp';
+
+function App() {
+  return (
+    <div className="App">
+      <OnePalmApp />
+    </div>
+  );
+}
+
+export default App;
